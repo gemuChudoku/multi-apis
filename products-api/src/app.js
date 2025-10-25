c13a0b610eb3217d8f691a65d5c2635869a2daed
Carlos Losada <Carloslosadadiaz2005@gmail.com>
2025-10-24 19:49:01 -0500

    prueba de conexion de product 2222

--- products-api/src/app.js
+++ products-api/src/app.js
@@ -117,5 +117,5 @@ app.delete("/products/:id", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`✅ ${SERVICE} listening on http://localhost:${PORT}`);
   console.log(`↔️  USERS_API_URL=${USERS_API_URL}`);
-  console.log(`🗄️  MONGODB_URL=${DATABASE_URL}`);
+  console.log(`🗄️  MONGODB_URL=${DATABASE_URL}`); //hola
 });
